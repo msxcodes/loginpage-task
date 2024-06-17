e211ea5941d8d78eb95311195b54e5bacb671719
Mohit Singh <msxcodes@msxcodes.kali>
2024-06-17 09:55:21 +0530

    initial commit

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,111 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Poster from "../../public/bg.jpg";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-wrap">
+      <div className="flex w-full flex-col md:w-[30%] h-screen bg-slate-100">
+        <div className="lg:w-[28rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:px-6 md:pt-0">
+          <p className="text-center text-2xl font-bold text-blue-600 ">
+            Login to your account
+          </p>
+          <form className="flex flex-col pt-3 md:pt-2">
+            <div className="flex flex-col pt-4">
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="mb-4 font-bold inline-block text-sm text-black"
+                >
+                  Enter Email Address
+                </label>
+                <input
+                  type="text"
+                  className="block w-full cursor-text appearance-none bg-transparent rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow placeholder:text-gray-600"
+                  id="email"
+                  name="email-username"
+                  placeholder="Email *"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col pt-4">
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="mb-4 font-bold inline-block text-sm text-black"
+                >
+                  Enter Your Password
+                </label>
+                <input
+                  type="text"
+                  className="block w-full cursor-text appearance-none rounded-md border bg-transparent border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-indigo-500 focus:bg-white focus:text-gray-600 focus:shadow placeholder:text-gray-600"
+                  id="email"
+                  name="email-username"
+                  placeholder="Password *"
+                />
+              </div>
+            </div>
+            <Link href={"#"} className="mb-4 mt-6">
+              {" "}
+              <span className="text-sm text-blue-700 hover:underline">
+                Forgot Password?
+              </span>{" "}
+            </Link>
+            <button
+              type="submit"
+              className="w-max mx-auto border border-blue-600 text-blue-600 hover:bg-blue-600 px-4 py-1.5 text-center text-sm uppercase hover:text-white transition-all duration-300 ring-gray-500 ring-offset-2 rounded-sm focus:ring-2"
+            >
+              Log in
+            </button>
+          </form>
+          <Link href={"#"} className="py-6 text-left">
+            <p className="whitespace-nowrap text-sm text-black hover:text-blue-600 hover:underline">
+              Create a free account?
+            </p>
+          </Link>
+        </div>
+      </div>
+      <div className="pointer-events-none relative hidden h-screen select-none bg-black md:block md:w-[70%]">
+        <div className="absolute z-10 px-8 text-black opacity-100 top-8 w-[55%] left-12">
+          <h2 className="mb-8 text-[1.75rem] font-bold leading-7">
+            Heading Of The Login Page And It Is The Demo Heading
+          </h2>
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+            quisquam, quam laboriosam quibusdam nisi totam harum quae cupiditate
+            illo, enim consequatur? Expedita commodi doloribus tempore, culpa at
+            facere voluptates sint aut ut voluptatibus fugit placeat nam
+            similique magni eveniet ipsa quod velit eos temporibus. Quae sit
+            voluptatum suscipit totam commodi?
+          </p>
+          <button className=" flex items-center justify-center space-x-2 cursor-pointer mt-4 py-2 font-light text-black text-xs transition hover:text-blue-500">
+            <span className="border-b-4 border-gray-700 pb-1 hover:text-blue-500">
+              {" "}
+              Learn more{" "}
+            </span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+        <Image
+          className="-z-1 absolute top-0 h-full w-full object-cover"
+          src={Poster}
+          alt="poster"
+        />
+      </div>
+    </div>
+  );
+}
